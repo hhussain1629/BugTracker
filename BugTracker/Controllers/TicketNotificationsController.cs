689800833f7584bf90652018e4111a216db9b4bd
hhussain1629 <hhussain1629@gmail.com>
2015-08-21 17:04:40 -0400

    8-21-15

---
+++ BugTracker/Controllers/TicketNotificationsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BugTracker.Models;
+
+namespace BugTracker.Controllers
+{
+    public class TicketNotificationsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: TicketNotification
+        [Authorize (Roles="Developer")]
+        public ActionResult Index(string id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var notifications = db.TicketNotifications.Where(tn => tn.Ticket.AssignedToUserId == id).ToList();
+            ViewBag.DisplayName = db.Users.FirstOrDefault(u => u.Id == id).DisplayName;
+            return View(notifications);
+        }
+    }
+}

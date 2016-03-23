be952204588f92369e594f7905dc1f8b08171064
hhussain1629 <hhussain1629@gmail.com>
2016-03-23 18:37:29 -0400

    3-23-16
    --In the Web.Config file in the root directory, in the <appSettings>
    block, the “value” string for the “SendGridPassword” key must be filled
    in.
    
    --The code is for the online version. To run on the desktop, do the
    following.  (1) In the Web.Config file in the root directory, in the
    <connectionStrings> block, comment out the “HussainsDB” entry. (2) In
    the “IdentityModels.cs” file in the “Models” folder, comment out the
    “HussainsDB” base (with “public ApplicationDbContext” above) and
    uncomment the “DefaultConnection” base (with “public
    ApplicationDbContext” above).

--- BugTracker/Models/IdentityModels.cs
+++ BugTracker/Models/IdentityModels.cs
@@ -42,10 +42,10 @@ namespace BugTracker.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
         //public ApplicationDbContext()
-        //  : base("HussainsDB", throwIfV1Schema: false)
+        //   : base("DefaultConnection", throwIfV1Schema: false)
+        public ApplicationDbContext()
+          : base("HussainsDB", throwIfV1Schema: false)
         {
         }
 
